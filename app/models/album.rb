955ca40e7f8014a0c0594b88ea84b60cdc68b3bb
ravi <rshri@gmail.com>
2016-03-05 14:09:40 +0530

    Task #6 added presence validation for album's publisher

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,6 +3,6 @@ class Album < ActiveRecord::Base
   has_many :songs
   
   validates :name, presence: true, length: { in: 2..50 }
-  validates :cover_art, presence: true
+  validates :cover_art,:publisher_id, presence: true
   validates :released_on, presence: true, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: "should be in the format YYYY-MM-DD" }
 end
