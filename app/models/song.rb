91d4e834494a5b0fefd7696f566d6347e7553c89
ravi <rshri@gmail.com>
2016-03-05 14:02:03 +0530

    Task #4 In the song model, added scope called recent that takes an argument n and returns songs released during the past 6 months

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,4 +4,8 @@ class Song < ActiveRecord::Base
   
   validates :name, presence: true, length: { in: 5..50 }
   validates :album_id, :artist_id, presence: true
+
+ scope :recent, -> (n) { joins(:album).where("albums.released_on > ?", 6.months.ago).
+  	order("albums.released_on DESC").order("songs.id DESC").limit(n) } 
+
 end
