01085db98714f81aad0e36dfda38b4d72215645e
ravi <rshri@gmail.com>
2016-03-05 16:19:11 +0530

    Task #12 Enabled data in json format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html
+  respond_to :html,:json
   before_action :get_latest_data
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   def get_latest_data
-     @latest_song = Song.joins(:album).order("albums.released_on DESC").order("songs.id DESC").first
+     @latest_song =  Song.recent(1).first
   end
 end
